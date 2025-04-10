8385c6cdbd0774dad3965ebe07ad1b4a4a525e8d
kalyanAyyagari <ayyagarishankarkalyan@gmail.com>
2025-04-10 22:25:14 +0530

    Add: header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BreadcrumbComponent } from "./shared/components/breadcrumb/breadcrumb.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BreadcrumbComponent],
+  imports: [RouterOutlet, BreadcrumbComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
