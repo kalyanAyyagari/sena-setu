b944077667df9b6b73665be604c48941536990dd
kalyanAyyagari <ayyagarishankarkalyan@gmail.com>
2025-04-14 22:50:52 +0530

    Update: subproduct form

--- src/app/feature/sub-product/add-sub-products/add-sub-products.component.ts
+++ src/app/feature/sub-product/add-sub-products/add-sub-products.component.ts
@@ -82,7 +82,7 @@ export class AddSubProductsComponent {
 
   incrementQuantity() {
     const currentQuantity = this.addSubproductForm.get('quantity')?.value || 0;
-    this.addSubproductForm.get('quantity')?.setValue(currentQuantity + 1);
+    this.addSubproductForm.get('quantity')?.setValue(+currentQuantity + 1);
   }
 
   decrementQuantity() {
@@ -97,5 +97,23 @@ export class AddSubProductsComponent {
       quantity: 0,
       barcode: ''
     };
+  }
+
+  onlyNumbers(event: KeyboardEvent): boolean {
+    // Allow: backspace, delete, tab, escape, enter, decimal point
+    if ([46, 8, 9, 27, 13, 190].includes(event.keyCode) ||
+      // Allow: Ctrl+A
+      (event.keyCode === 65 && event.ctrlKey === true) ||
+      // Allow: home, end, left, right
+      (event.keyCode >= 35 && event.keyCode <= 39)) {
+      return true;
+    }
+    // Ensure that it is a number and stop the keypress if not
+    if ((event.shiftKey || (event.keyCode < 48 || event.keyCode > 57)) &&
+      (event.keyCode < 96 || event.keyCode > 105)) {
+      event.preventDefault();
+      return false;
+    }
+    return true;
   }
 }
