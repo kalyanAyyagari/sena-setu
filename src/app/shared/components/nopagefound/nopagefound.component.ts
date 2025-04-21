43e1485ec7161ec4e63cd716105b892ea50e8bc6
kalyanAyyagari <ayyagarishankarkalyan@gmail.com>
2025-04-21 22:08:01 +0530

    Update: no page found and few css

--- src/app/shared/components/nopagefound/nopagefound.component.ts
+++ src/app/shared/components/nopagefound/nopagefound.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nopagefound',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './nopagefound.component.html',
   styleUrl: './nopagefound.component.scss'
 })
 export class NopagefoundComponent {
+  constructor(private router: Router) {}
 
+  goHome(): void {
+    this.router.navigate(['/units']);
+  }
 }
